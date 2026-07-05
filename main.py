c9e1d040b8548d210834c4063195e827b64fb4fa
Ani-Human <human_ani@proton.me>
2026-07-05 09:03:50 +0530

    Simple name joiner program

---
+++ main.py
@@ -0,0 +1,5 @@
+def get_full_name(first_name, last_name): #The function and its parameters
+    full_name= first_name + " " + last_name #It takes the first name and last name. Also adds a space in between.
+    return full_name #Returns the output (only for the machine not the user)
+
+print(get_full_name('john', 'doe')) #prints the output with the paramerters 'john' and 'doe'.
