4dca0496cfb79e72851fa8d563bc2a6f1d733251
Ani-Human <human_ani@proton.me>
2026-07-05 09:29:02 +0530

    get_name_with_age program, type hints and data conversion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def get_full_name(first_name: str, last_name: str):         #The function with the parameters equipped with type hints (str)
-    full_name= first_name.title() + ' ' + last_name.title()  #String concatenation, giving the parameters a way to express them selves. Used .title() to capitalize the first letter.
-    return full_name                                        #Returning the output
+def get_name_with_age(name: str, age: int):                   #Type hints declared but unsupported combination (str + int)
+    name_with_age = name + " is this old: " + str(age)        #Converted age to str for compatibiltiy.
+    return name_with_age
 
-print(get_full_name('john', 'doe'))
-
-#type hints are used for annotations for variables, to declare the type of data they give.
+print(get_name_with_age('ani', 18))
