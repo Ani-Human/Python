807cb17cb9894e7870b38b90ebf12736c7a3574c
Ani-Human <human_ani@proton.me>
2026-07-06 14:35:07 +0530

    Type hints with various data types

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-def get_items(item_a: str, item_b: bool, item_c: int, item_d: bytes, item_e: float):
-    return item_a, item_b, item_c, item_d, item_e
+#Generic Type Perimeters
+#THIS METHOD HELPS TO CATCH BUGS EARLIER, and auto completions.
 
-#Type hints can have any data type.
+#LIST
+def process_stuff (thing: list[str]):   #Items would be a list of strings with type parameters.
+    for letter in thing:
+        print(letter)
+
+
+#SETS AND TUPLES
+def process_stuff_2(thing_1:tuple[int, int, str], thing_2: set[bytes]):
+    return (thing_1, thing_2)
+
+#DICTIONARY
+def process_stuff_3(prices: dict[str, float]):
+    for item_name, item_price in prices.items():
+        print(item_name)
+        print(item_price)
+# process_stuff_3({'bag' : 4.99})
+
+#Union
+def process_stuff_4(item: int | str):
+    print (item)
+
+#Possibly NONE
+def say_hi(name: str | None = None):
+    if name is not None:
+        print(f"Hi {name}")
+    else:
+        print("duh")
