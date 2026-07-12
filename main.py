e91e53ce646141486705f14ff32b3c8bdc62d87d
Ani-Human <human_ani@proton.me>
2026-07-12 09:55:57 +0530

    A simple fastapi program

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-#env variables
-#External to python
+from fastapi import FastAPI  #Importing the fastapi module
 
+app = FastAPI()  #Creating a fastapi instance [app variable will be an instance of Fastapi]
 
-import os
-from dotenv import load_dotenv
+@app.get('/')  #Path operation (home) | @something syntax is called a 'decorator', it takes a function below and does something with it.
+async def root():  #The async function. Can be normal too.
+    return {'message' : 'Hello'}  #Returning the content. A dict, list, str or int can be returned too. 
 
-load_dotenv()
+#-----FastAPI operations----------
+'''
+(normal)
+-Post: to create data.
+-Get: to read data.
+-Put: to update data.
+-Delete: to delete data.
 
-name = os.getenv("NAME")
-print(f'Hola {name}')
+(Exotic)
+-Options
+-Head
+-Patch
+-Trace
+'''
 
-
-#--------Only with os and export of env vars--------
-#  source myenv/bin/activate
-# (myenv) export MY_NAME="WADE_THE_GREAT"
-# (myenv) python main.py
-# Hola WADE_THE_GREAT
-# (myenv) MY_NAME="Habibi" python3 main.py
-# Hola Habibi
-# (myenv) python main.py
-# Hola WADE_THE_GREAT
-
-
-#--------With load_dotenv and os--------
-
-# (myenv) python main.py
-# Hola IWONTTELLU
+# Recap¶
+# Import FastAPI.
+# Create an app instance.
+# Write a path operation decorator using decorators like @app.get("/").
+# Define a path operation function; for example, def root(): ....
+# Run the development server using the command fastapi dev.
+# Optionally deploy your app with fastapi deploy.
