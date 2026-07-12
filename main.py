f48bb35c8dcba7833fbcb899a82e913ee559eaf9
Ani-Human <human_ani@proton.me>
2026-07-12 17:45:05 +0530

    Path param's order matters

--- main.py
+++ main.py
@@ -2,7 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-#path parameters
-@app.get('/item_id/{item_id}')
-async def item(item_id: int):
-    return {'item_ID' : item_id}
+#The order matters alot.abs
+#path operations are evaluated in order. 
+
+
+@app.get('/users/me')
+async def read_user_me():
+    return {'user' : 'me'} #This was using the read_user function and bugged. 
+
+@app.get('/users/{user_id}')
+async def read_user(user_id: int):
+    return {'user' : user_id}
+
+
+
+'''
+The order matters becuz if the position of the functions are reversed,
+the user_id parameter will have the result 'me' always.
+[Paths are evaluated in order]
+
+path for /users/{user_id} would match also for /users/me, 
+"thinking" that it's receiving a parameter user_id with a value of "me"
+'''
