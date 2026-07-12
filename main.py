68df3fd20ffff9f1a722fbf6f4c3ffddaa464cef
Ani-Human <human_ani@proton.me>
2026-07-12 07:47:03 +0530

    Env var stuff

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-from typing import Annotated
+#env variables
+#External to python
 
-def say_hello(name: Annotated[str, 'this is just metadata']) -> str:
-    return f"hello {name}"
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+name = os.getenv("NAME")
+print(f'Hola {name}')
+
+
+#--------Only with os and export of env vars--------
+#  source myenv/bin/activate
+# (myenv) export MY_NAME="WADE_THE_GREAT"
+# (myenv) python main.py
+# Hola WADE_THE_GREAT
+# (myenv) MY_NAME="Habibi" python3 main.py
+# Hola Habibi
+# (myenv) python main.py
+# Hola WADE_THE_GREAT
+
+
+#--------With load_dotenv and os--------
+
+# (myenv) python main.py
+# Hola IWONTTELLU
