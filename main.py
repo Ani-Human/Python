b386e76e4f08879dfa18c7bfa8529ad2a0605e03
Ani-Human <human_ani@proton.me>
2026-07-12 17:11:02 +0530

    Fastapi code with path params

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-from fastapi import FastAPI  #Importing the fastapi module
+from fastapi import FastAPI
 
-app = FastAPI()  #Creating a fastapi instance [app variable will be an instance of Fastapi]
+app = FastAPI()
 
-@app.get('/')  #Path operation (home) | @something syntax is called a 'decorator', it takes a function below and does something with it.
-async def root():  #The async function. Can be normal too.
-    return {'message' : 'Hello'}  #Returning the content. A dict, list, str or int can be returned too. 
-
-#-----FastAPI operations----------
-'''
-(normal)
--Post: to create data.
--Get: to read data.
--Put: to update data.
--Delete: to delete data.
-
-(Exotic)
--Options
--Head
--Patch
--Trace
-'''
-
-# Recap¶
-# Import FastAPI.
-# Create an app instance.
-# Write a path operation decorator using decorators like @app.get("/").
-# Define a path operation function; for example, def root(): ....
-# Run the development server using the command fastapi dev.
-# Optionally deploy your app with fastapi deploy.
+#path parameters
+@app.get('/item_id/{item_id}')
+async def item(item_id: int):
+    return {'item_ID' : item_id}
