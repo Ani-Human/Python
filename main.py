b003034fa821b119c00ae0c735151604e45b9138
Ani-Human <human_ani@proton.me>
2026-07-15 09:57:21 +0530

    The whole path param and query param stuff (all cleared)

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/items/{item_id}')
-async def read_user_item(item_id: str, required_query: int ): #To make the required query optional add " | None = None"
-    return {
-        'item id' : item_id,
-        'Required query' : required_query,
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {
+        'item id' : item_id, #path parameter
+        'needy' : needy, #Required query parameter
+        'skip' : skip, #query parameter with default value 0
+        'limit' : limit, #Option query parameter
     }
+    return item
+
+#Enum can also be used in this case!
