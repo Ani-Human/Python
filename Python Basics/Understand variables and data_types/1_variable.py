1d2353736251e2b373b5a7cd6320000cf447ff85
Ani-Human <human_ani@proton.me>
2026-08-23 12:45:06 +0530

    strenghtening python

---
+++ Python Basics/Understand variables and data_types/1_variable.py
@@ -0,0 +1,17 @@
+name = 'john doe'
+age = 25
+
+'''---------
+Variable naming rules:
+- Starts with letter or underscore_
+- can contain alphanumeric characters
+- very case sensitive
+- cant be python's reserved words
+Brearking these will give sytax error
+-------------'''
+
+my_variable_name = 'freeCodeCamp' #snake case naming convention
+
+#Tip: Use descriptive names for variables | no use of single letter variable
+user_age = 30 #not ua, age etc
+
---
+++ Python Basics/Understand variables and data_types/2_print_function.py
@@ -0,0 +1,10 @@
+#in the terminal outputs can be seen 
+
+#print() function: Built-in function | Sends text and other values to the terminal.
+
+print('Hello World!')
+#the string 'Hello World!' is an argument passed to the print function.
+
+#print function can also be used to show multiple aruguments/values at once by separating them with commas
+print('HI!', 'i', 'am', 'ani!')
+#python automatically adds space when you separate the arguments with commas.
